85aa0bdfc8fd69e8fff94e6196add47467339039
Ingrid Kakou <ingridkakou@hotmail.fr>
2020-05-03 13:45:51 +0200

    debut projet

---
+++ projet/src/main/java/com/ACTOREP/demo/ProjetApplicationMain.java
@@ -0,0 +1,13 @@
+package com.ACTOREP.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetApplicationMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetApplicationMain.class, args);
+	}
+
+}

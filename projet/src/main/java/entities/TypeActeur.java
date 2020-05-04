ee27f57ba239a791a52ec3e9e24a8f33dd99bd69
Ingrid Kakou <ingridkakou@hotmail.fr>
2020-05-04 13:27:20 +0200

    modif typeActeur

--- projet/src/main/java/entities/TypeActeur.java
+++ projet/src/main/java/entities/TypeActeur.java
@@ -1,5 +1,21 @@
 package entities;
 
+import java.util.Collection;
+
+import javax.persistence.*;
+
+
+@Entity
 public class TypeActeur {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nom;
+	
+	private String description;
+	
+	private Collection<Compte> comptes;
 
 }

7a4736980beb90f2fa1b007a73ab4ac40d0eb339
Dondealex <naudin.alexandre@gmail.com>
2020-05-04 16:27:58 +0200

    classe alex

--- projet/src/main/java/entities/Acteur.java
+++ projet/src/main/java/entities/Acteur.java
@@ -1,5 +1,47 @@
 package entities;
 
-public class Acteur {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Acteur {
+	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+	@Column(name = "Nom", length = 75, nullable = false)
+	private String nom;
+	
+	@Column(name = "Description", length = 300,nullable =false)
+	private String description;
+
+public Acteur(String nom, String description) {
+	this.nom = nom;
+	this.description=description;
+	
+}
+public Acteur() {
+	super();
+}
+
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+@Override
+public String toString() {
+	return "Acteur [id=" + id +"Nom=  "+ nom + ", Description=" + description + "]";
+}
 }
--- projet/src/main/java/entities/Categories.java
+++ projet/src/main/java/entities/Categories.java
@@ -1,5 +1,47 @@
 package entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categories {
+	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+	@Column(name = "Nom", length = 75, nullable = false)
+	private String nom;
+	
+	@Column(name = "Description", length = 300,nullable =false)
+	private String description;
+	public Categories(String nom, String description) {
+		this.nom = nom;
+		this.description=description;
+		
+	}
+	public Categories() {
+		super();
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "Acteur [id=" + id +"Nom=  "+ nom + ", Description=" + description + "]";
+	}
 
 }

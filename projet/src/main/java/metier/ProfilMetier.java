bd0c5bf45e068b2a4a99487fd80bb26199ea632d
Nawel <nl.boumediene@gmail.com>
2020-05-09 12:38:59 +0200

    Modification de l'arborescence du projet

---
+++ projet/src/main/java/metier/ProfilMetier.java
@@ -0,0 +1,19 @@
+package metier;
+
+import java.io.Serializable;
+import entities.Profil;
+import repository.ProfilRepository;
+
+public class ProfilMetier implements Serializable{
+	
+	ProfilRepository profilRep;
+	
+	public void saveProfil(Profil profil) {
+		profilRep.save(profil);
+	}
+	
+	
+	
+	
+
+}

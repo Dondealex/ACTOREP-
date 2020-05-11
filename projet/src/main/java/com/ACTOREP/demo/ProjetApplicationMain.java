84bd51813ace3189513091f0e9d740b3d05bad8b
Dany <ionformat@gmail.com>
2020-05-11 11:16:44 +0200

    main conf resolu

--- projet/src/main/java/com/ACTOREP/demo/ProjetApplicationMain.java
+++ projet/src/main/java/com/ACTOREP/demo/ProjetApplicationMain.java
@@ -10,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import repository.DepartementRepository;
 
 import entities.Administrateur;
 import entities.ReseauSocial;
@@ -18,22 +21,23 @@ import repository.AdminDaoImpl;
 import repository.ResSocDaoImpl;
 import repository.StatutDaoImpl;
 
-
 @SpringBootApplication
-@ComponentScan (basePackages = {"repository", "controller", "metier", "session"})
-@EntityScan (basePackages = {"entities"})
+@ComponentScan(basePackages = {"repository","controller","metier","session"})
+@EntityScan(basePackages = {"entities"})
+@EnableJpaRepositories(basePackageClasses = DepartementRepository.class)
+
 public class ProjetApplicationMain {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetApplicationMain.class, args);
 	}
-	
+
 @Autowired
 AdminDaoImpl amd;
 
 @Autowired
 StatutDaoImpl std;
-
+	
 @Autowired
 ResSocDaoImpl rsd;
 	
